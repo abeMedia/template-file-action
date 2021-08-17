80687ba9c93dc86ce6dfcd91416160f2df3aefe6
Adam Bouqdib <adam@abemedia.co.uk>
2021-08-17 18:44:32 +0100

    fix: preserve indentation level of close delimiter

--- __tests__/inject.test.ts
+++ __tests__/inject.test.ts
@@ -25,6 +25,18 @@ test('injects between comment hash', async () => {
   expect(inject(input, 'bar')).toEqual(expected)
 })
 
+test('preserves whitespace in front of comments', async () => {
+  const input = ` # START_TEMPLATE\nfoo\n # END_TEMPLATE`
+  const expected = ` # START_TEMPLATE\nbar\n # END_TEMPLATE`
+  expect(inject(input, 'bar')).toEqual(expected)
+})
+
+test("doesn't preserve whitespace following characters in front of comments", async () => {
+  const input = ` # START_TEMPLATE\nfoo\nbar # END_TEMPLATE`
+  const expected = ` # START_TEMPLATE\nbar\n# END_TEMPLATE`
+  expect(inject(input, 'bar')).toEqual(expected)
+})
+
 test('replaces content without delimiters', async () => {
   const input = `foo`
   const expected = `bar`
