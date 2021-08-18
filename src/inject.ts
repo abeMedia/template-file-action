f3729605ded80945cad56b597c72597ea1966c86
Adam Bouqdib <adam@abemedia.co.uk>
2021-08-18 09:34:15 +0100

    fix: preserve indentation level of close delimiter (#11)
    * fix: preserve indentation level of close delimiter
    
    * chore: build
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/inject.ts
+++ src/inject.ts
@@ -1,9 +1,19 @@
-export const reDelimiter = new RegExp(
-  '(?<=<!-- START_TEMPLATE -->)[\\s\\S]*(?=<!-- END_TEMPLATE -->)|' +
-    '(?<=\\/\\* START_TEMPLATE \\*\\/)[\\s\\S]*(?=\\/\\* END_TEMPLATE \\*\\/)|' +
-    '(?<=\\/\\/ START_TEMPLATE)[\\s\\S]*(?=\\/\\/ END_TEMPLATE)|' +
-    '(?<=# START_TEMPLATE)[\\s\\S]*(?=# END_TEMPLATE)',
-)
+type Commenter = (s: string) => string
+
+const comments: Commenter[] = [
+  (s) => `<!-- ${s} -->`,
+  (s) => `\\/\\* ${s} \\*\\/`,
+  (s) => `\\/\\/ ${s}`,
+  (s) => `# ${s}`,
+]
+
+function regex(comment: Commenter) {
+  const start = comment('START_TEMPLATE')
+  const end = comment('END_TEMPLATE')
+  return `(?<=${start})[\\s\\S]*?(?=(^[^\\S\\r\\n]*)?${end})`
+}
+
+export const reDelimiter = new RegExp(comments.map(regex).join('|'), 'm')
 
 export function inject(content: string, text: string): string {
   if (reDelimiter.test(content)) {
